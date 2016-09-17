wking/image-spec

image/refs: Implement Engine.Put
This is pretty straightforward with the new WriteTarEntryByName
helper.

I considered pulling the ref name -> path conversion (%s -> ./refs/%s)
out into a helper function to stay DRY, but the logic is simple enough
that it seemed more intuitive to leave it inline.

Signed-off-by: W. Trevor King <wking@tremily.us>

image/refs/layout/tar.go
@@ -16,6 +16,7 @@ package layout
 
 import (
 	"archive/tar"
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -25,6 +26,7 @@ import (
 
 	casLayout "github.com/opencontainers/image-spec/image/cas/layout"
 	imageLayout "github.com/opencontainers/image-spec/image/layout"
+	utilLayout "github.com/opencontainers/image-spec/image/layout"
 	"github.com/opencontainers/image-spec/image/refs"
 	"github.com/opencontainers/image-spec/specs-go"
 	"golang.org/x/net/context"
@@ -51,8 +53,15 @@ func NewTarEngine(ctx context.Context, file casLayout.ReadWriteSeekCloser) (eng 
 
 // Put adds a new reference to the store.
 func (engine *TarEngine) Put(ctx context.Context, name string, descriptor *specs.Descriptor) (err error) {
-	// FIXME
-	return errors.New("TarEngine.Put is not supported yet")
+	data, err := json.Marshal(descriptor)
+	if err != nil {
+		return err
+	}
+
+	size := int64(len(data))
+	reader := bytes.NewReader(data)
+	targetName := fmt.Sprintf("./refs/%s", name)
+	return utilLayout.WriteTarEntryByName(ctx, engine.file, targetName, reader, &size)
 }
 
 // Get returns a reference from the store.

wking/image-tools

image/refs/layout/tar.go
@@ -16,6 +16,7 @@ package layout
 
 import (
 	"archive/tar"
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -51,8 +52,15 @@ func NewTarEngine(ctx context.Context, file caslayout.ReadWriteSeekCloser) (eng 
 
 // Put adds a new reference to the store.
 func (engine *TarEngine) Put(ctx context.Context, name string, descriptor *specs.Descriptor) (err error) {
-	// FIXME
-	return errors.New("TarEngine.Put is not supported yet")
+	data, err := json.Marshal(descriptor)
+	if err != nil {
+		return err
+	}
+
+	size := int64(len(data))
+	reader := bytes.NewReader(data)
+	targetName := fmt.Sprintf("./refs/%s", name)
+	return imagelayout.WriteTarEntryByName(ctx, engine.file, targetName, reader, &size)
 }
 
 // Get returns a reference from the store.